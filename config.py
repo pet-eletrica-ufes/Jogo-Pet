491bd547669717b159b5b85c6c0e6074b697963b
pet-eletrica-ufes <82731623+pet-eletrica-ufes@users.noreply.github.com>
2024-09-22 10:28:55 -0300

    atualização no config para percentual da tela

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ screen_width = 600  # Exemplo de largura da tela
 screen_height = 371  # Exemplo de altura da tela
 # padrão 600x371
 # Coordenadas iniciais do personagem
-initial_x_pos = 20
-initial_y_pos = 480
+initial_x_pos = screen_width*0.0334
+initial_y_pos = screen_height*0.9
 
 # Variáveis de ambiente 
 velo=3.5
