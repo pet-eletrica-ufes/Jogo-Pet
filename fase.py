ecaa628c45e7c79d7156d3a085a1d0f38243a213
pet-eletrica-ufes <82731623+pet-eletrica-ufes@users.noreply.github.com>
2024-09-21 21:30:14 -0300

    Subindo sprites

--- fase.py
+++ fase.py
@@ -2,12 +2,14 @@ import pygame
 
 # Função para carregar o plano de fundo e outros objetos da fase 1
 def carregar_fase_1():
-    background_image = pygame.image.load('plano de fundo.jpeg').convert()
+    background_image = pygame.image.load('Sprites/plano de fundo.jpeg').convert()
     objetos_fase = []
 
     # Carrega objetos da fase 1
     plataforma = pygame.Rect(150, 450, 250, 20)  # Exemplo de uma plataforma
-    objetos_fase.append(plataforma)
+    lampada_image = pygame.image.load('Sprites/lampadas_desligada.png').convert()  # Usa convert() para otimizar
+    lampada_image.set_colorkey((0, 0, 0))  # Define a cor preta como transparente (R: 0, G: 0, B: 0)
+    objetos_fase.append(lampada_image)
 
     return background_image, objetos_fase
 
@@ -17,5 +19,5 @@ def desenhar_fase(screen, background_image, objetos_fase):
     screen.blit(background_image, (0, 0))
 
     # Desenha os objetos da fase (ex: plataformas)
-    #for objeto in objetos_fase:
-        #pygame.draw.rect(screen, (255, 0, 0), objeto)  # Desenha as plataformas em vermelho
+    for objeto in objetos_fase:
+        screen.blit(objeto, (100, 100))  # Desenha as plataformas em vermelho
